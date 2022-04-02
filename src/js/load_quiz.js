5898047a41249fb3b66ab8af0d6a6b8f5c1a95df
martin.danek <martin.danek@dedoles.sk>
2022-04-02 21:04:33 +0200

    labels

---
+++ src/js/load_quiz.js
@@ -0,0 +1,71 @@
+const quiz_questions = [
+    {
+        "name": "quiz1",
+        "right_answer": "30 688",
+        "text": "How big is the belgium ? (in km2)",
+        "options": [
+            {
+                "name":"image_value1",
+                "value": "25 320",
+                
+            },
+            {
+                "name":"image_value1",
+                "value": "55 144",
+            }
+            ,
+            {
+                "name":"image_value1",
+                "value": "30 688",
+            }
+        ]
+    },
+    {
+        "name": "quiz2",
+        "right_answer": "30 688",
+        "text": "How big is the belgium ? (in km2)",
+        "options": [
+            {
+                "name":"image_value1",
+                "value": "25 320",
+                
+            },
+            {
+                "name":"image_value1",
+                "value": "55 144",
+            }
+            ,
+            {
+                "name":"image_value1",
+                "value": "30 688",
+            }
+        ]
+    },
+    {
+        "name": "quiz3",
+        "right_answer": "30 688",
+        "text": "How big is the belgium ? (in km2)",
+        "options": [
+            {
+                "name":"image_value1",
+                "value": "25 320",
+                
+            },
+            {
+                "name":"image_value1",
+                "value": "55 144",
+            }
+            ,
+            {
+                "name":"image_value1",
+                "value": "30 688",
+            }
+        ]
+    },
+   
+        
+    
+];
+
+
+
