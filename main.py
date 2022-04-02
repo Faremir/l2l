d067f046f02e687c50b04b1dcab06ce03e1a4a12
faremir <1363005+Faremir@users.noreply.github.com>
2022-04-02 15:50:11 +0200

    [UPDATE] Simple HTTP Server

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.server
+import socketserver
 
-hostName = "localhost"
-serverPort = 8080
-
-
-class MyServer(BaseHTTPRequestHandler):
-    def url_to_path(self ):
-        print(self.path)
-        pass
-
-    def do_GET(self):
-        self.send_response(200)
-        self.url_to_path()
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(
-            bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+PORT = 8000
+DIRECTORY = "src"
 
 
-if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
 
-    webServer.server_close()
-    print("Server stopped.")
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
