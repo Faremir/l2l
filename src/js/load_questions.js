dffa1bbc69452a096bf29e124674376fc2c1d3c4
Daniel Lagin <daniel.lagin@gymbeam.com>
2022-04-02 23:15:14 +0200

    [ADD] som pushol data 3000

--- src/js/load_questions.js
+++ src/js/load_questions.js
@@ -1,12 +1,12 @@
 const questions = [
     {
-        "name": "Image1",
+        "name": "Picture",
         "category": "visual",
-        "text": "Whats brings you a joy?",
+        "text": "Choose one picture.",
         "options": [
             {
                 "name":"image_value1",
-                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "value": "https://thumbs.dreamstime.com/b/rorschach-inkblot-test-illustration-random-symmetrical-ink-abstract-ink-stain-psycho-diagnostic-inkblot-rorschach-projection-164249184.jpg",
                 "visual_value": 4,
                 "static_value": 1,
                 "emotional_value": 3,
@@ -14,7 +14,7 @@ const questions = [
             },
             {
                 "name":"image_value2",
-                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "value": "https://previews.123rf.com/images/belopoppa/belopoppa2004/belopoppa200400009/144339246-rorschach-inkblot-test-illustration-random-symmetrical-abstract-ink-stains-psycho-diagnostic-for-ink.jpg",
                 "visual_value": 2,
                 "static_value": 1,
                 "emotional_value": 3,
@@ -23,7 +23,7 @@ const questions = [
             ,
             {
                 "name":"image_value3",
-                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "value": "https://cdn.w600.comps.canstockphoto.com/rorschach-inkblot-test-illustration-eps-vector_csp66312181.jpg",
                 "visual_value": 3,
                 "static_value": 1,
                 "emotional_value": 3,
@@ -32,98 +32,97 @@ const questions = [
         ]
     },
     {
-        "name": "Text1",
+        "name": "Book",
         "category": "static",
-        "text": "Which statement is the most related to you?",
+        "text": "What kind of book would you like to read for fun?",
         "options": [
             {
-                "name":"text_value1",
-                "value": "I am god",
+                "name":"pictures",
+                "value": "A book with lots of pictures in it",
                 "visual_value": 4,
                 "static_value": 1,
                 "emotional_value": 3,
                 "interactive_value": 2
             },
             {
-                "name":"text_value2",
-                "value": "I am devil",
-                "visual_value": 2,
-                "static_value": 1,
-                "emotional_value": 3,
+                "name":"words",
+                "value": "A book with lots of words in it",
+                "visual_value": 3,
+                "static_value": 4,
+                "emotional_value": 1,
                 "interactive_value": 2
             }
             ,
             {
-                "name":"text_value3",
-                "value": "I am god odoo developer",
+                "name":"puzzle",
+                "value": "A book with word searches or crossword puzzles",
                 "visual_value": 3,
                 "static_value": 1,
-                "emotional_value": 3,
-                "interactive_value": 2
+                "emotional_value": 2,
+                "interactive_value": 4
             }
         ]
     },
     {
-        "name": "Stain1",
-        "category": "emotional",
-        "text": "Which image scares you the most?",
+        "name": "Spelling",
+        "category": "interactive",
+        "text": "When you are not sure how to spell a word, what are you most likely to do?",
         "options": [
             {
-                "name":"stain_value1",
-                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
-                "visual_value": 4,
-                "static_value": 1,
-                "emotional_value": 3,
-                "interactive_value": 2
+                "name":"write",
+                "value": "Write it down to see if it looks right",
+                "visual_value": 2,
+                "static_value": 4,
+                "emotional_value": 1,
+                "interactive_value": 3
             },
             {
-                "name":"stain_value2",
-                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
-                "visual_value": 2,
-                "static_value": 1,
-                "emotional_value": 3,
-                "interactive_value": 2
+                "name":"spell",
+                "value": "Spell it out loud to see if it sounds right",
+                "visual_value": 1,
+                "static_value": 2,
+                "emotional_value": 4,
+                "interactive_value": 3
             }
             ,
             {
-                "name":"stain_value3",
-                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "name":"trace",
+                "value": "Trace the letters in the air (finger spelling)",
                 "visual_value": 3,
                 "static_value": 1,
-                "emotional_value": 3,
-                "interactive_value": 2
+                "emotional_value": 2,
+                "interactive_value": 4
             }
         ]
     },
     {
         "name": "Options1",
         "category": "interactive",
-        "text": "What is best answer for question?",
+        "text": "When you see the word cat, what do you do first?",
         "options": [
             {
-                "name":"options_value1",
-                "value": "baby don't hurt me",
+                "name":"imagine",
+                "value": "Picture a cat in your mind",
                 "visual_value": 4,
                 "static_value": 1,
                 "emotional_value": 3,
                 "interactive_value": 2
             },
             {
-                "name":"options_value2",
-                "value": "no more",
-                "visual_value": 2,
-                "static_value": 1,
+                "name":"say",
+                "value": "Say the word cat to yourself",
+                "visual_value": 1,
+                "static_value": 4,
                 "emotional_value": 3,
                 "interactive_value": 2
-            }
-            ,
+            },
             {
-                "name":"options_value3",
-                "value": "what is loove",
+                "name":"think",
+                "value": "Think about being with a cat (petting it or hearing it purr)",
                 "visual_value": 3,
                 "static_value": 1,
-                "emotional_value": 3,
-                "interactive_value": 2
+                "emotional_value": 2,
+                "interactive_value": 4
             }
         ]
     }
