2f8f0cd28327a994c22c3847d2136f652a366b43
martin.danek <martin.danek@dedoles.sk>
2022-04-02 20:25:56 +0200

    generate questions and steps

---
+++ src/js/load_questions.js
@@ -0,0 +1,134 @@
+const questions = [
+    {
+        "name": "Image1",
+        "category": "visual",
+        "text": "Whats brings you a joy ?",
+        "options": [
+            {
+                "name":"image_value1",
+                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "visual_value": 4,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            },
+            {
+                "name":"image_value2",
+                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "visual_value": 2,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            }
+            ,
+            {
+                "name":"image_value3",
+                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "visual_value": 3,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            }
+        ]
+    },
+    {
+        "name": "Text1",
+        "category": "static",
+        "text": "Which statement is the most related to you ?",
+        "options": [
+            {
+                "name":"text_value1",
+                "value": "I am god",
+                "visual_value": 4,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            },
+            {
+                "name":"text_value2",
+                "value": "I am devil",
+                "visual_value": 2,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            }
+            ,
+            {
+                "name":"text_value3",
+                "value": "I am god odoo developer",
+                "visual_value": 3,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            }
+        ]
+    },
+
+    {
+        "name": "Stain1",
+        "category": "emotional",
+        "text": "Which image scares you the most ?",
+        "options": [
+            {
+                "name":"stain_value1",
+                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "visual_value": 4,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            },
+            {
+                "name":"stain_value2",
+                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "visual_value": 2,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            }
+            ,
+            {
+                "name":"stain_value3",
+                "value": "https://focusonbelgium.be/sites/default/files/styles/big_article_image/public/odoo.jpg?itok=RNgWFSlf",
+                "visual_value": 3,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            }
+        ]
+    },
+    {
+        "name": "Options1",
+        "category": "interactive",
+        "text": "What is best answer for question ? 'What is Love'",
+        "options": [
+            {
+                "name":"options_value1",
+                "value": "baby don't hurt me",
+                "visual_value": 4,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            },
+            {
+                "name":"options_value2",
+                "value": "no more",
+                "visual_value": 2,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            }
+            ,
+            {
+                "name":"options_value3",
+                "value": "what is loove",
+                "visual_value": 3,
+                "static_value": 1,
+                "emotional_value": 3,
+                "interactive_value": 2
+            }
+        ]
+    }
+];
+
+
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,70 @@
-(function ($) {
-	
-	// USE STRICT
-	"use strict";
+render_questions();
+var last_clicked = 0;
 
-	console.log("Danek")
-})(jQuery);
+function save_button() {
+    var timeNow = (new Date()).getTime();
+    let last_div = $('.step_class').last()
+    console.log(timeNow, last_clicked)
+    let step_time_consumed = document.createElement("span");
+    step_time_consumed.innerHTML = (timeNow - last_clicked) / 1000
+    console.log(last_div);
+    $(last_div).append(step_time_consumed);
+    lastClicked = timeNow;
+}
+
+function render_step() {
+    last_clicked = (new Date()).getTime();
+    let step_wrap = document.createElement("div");
+    step_wrap.className = "step_class";
+    let step_type_input = document.createElement("select");
+    let step_value_input = document.createElement("input");
+    let step_comment_input = document.createElement("input");
+
+    let options = ['seaching', 'reading', 'listening', 'writing']
+    options.forEach(option => {
+        let opt = document.createElement('option');
+        opt.value = option;
+        opt.innerHTML = option;
+        step_type_input.appendChild(opt);
+    });
+    $(step_wrap).append(step_type_input);
+    $(step_wrap).append(step_value_input);
+    $(step_wrap).append(step_comment_input);
+
+    $('#steps').append(step_wrap);
+
+}
+function render_questions() {
+    questions.forEach(
+        question => {
+            console.log(question.category);
+            $("#person").append("<br/><br/><br/><br/>");
+            question.options.forEach(option => {
+				let input_wrapper = document.createElement("div");
+                let input_element = document.createElement("input");
+                input_element.id = option.name;
+                input_element.type = "radio";
+                input_element.name = question.category;
+                input_element.value = option.name;
+                input_element.dataset.visual_value = question.visual_value;
+                input_element.dataset.static_value = question.static_value;
+                input_element.dataset.emotional_value = question.emotional_value;
+                input_element.dataset.interactive_value = question.interactive_value;
+                $(input_wrapper).append(input_element);
+                let label_element = document.createElement("label");
+                label_element.for = input_element.id;
+                if (["visual", "emotional"].includes(question.category)) {
+                    let img_element = document.createElement("img");
+                    img_element.src = option.value;
+					img_element.style.maxHeight="10px";
+                    $(label_element).append(img_element);
+                }
+                else {
+                    $(label_element).append(option.value);
+                }
+				$(input_wrapper).append(label_element);
+                $("#person").append(input_wrapper);
+            });
+
+        });
+}
