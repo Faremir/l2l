0444ddb2076fe71982dbcf5a31457399c1f2e81a
faremir <1363005+Faremir@users.noreply.github.com>
2022-04-02 15:56:18 +0200

    [UPDATE]

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import http.server
-import socketserver
 
+URL = ""
 PORT = 8000
 DIRECTORY = "src"
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+class StoppableHTTPServer(http.server.HTTPServer):
+
+    def run(self):
+        try:
+            self.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            # Clean-up server (close socket, etc.)
+            self.server_close()
+
+
+if __name__ == '__main__':
+    server = StoppableHTTPServer((URL, PORT), Handler)
+    server.run()
