45afab7e846d30eae7aa2e708e87b01af302c35e
faremir <1363005+Faremir@users.noreply.github.com>
2022-04-03 06:02:27 +0200

    [FIX]

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,9 @@
     function save_button() {
         var timeNow = (new Date()).getTime();
         let last_div = $('.step').last()
-        if ($(last_div).data().processed === "False" || !$(last_div).children('.step-value').val()) {
+        let is_processed = $(last_div).data().processed === "True";
+        let value_empty = !$(last_div).children('.step_value').val();
+        if (is_processed || value_empty) {
             alert("You need to fill the value field before saving!");
             return false;
         }
@@ -42,6 +44,7 @@
         step_value_label.innerHTML = "Value"
         let step_value_input = document.createElement("input");
         step_value_input.className = 'step_value';
+        step_value_input.value = "";
 
         let step_com_label = document.createElement("label");
         step_com_label.innerHTML = "Comment"
@@ -105,44 +108,42 @@
     function render_questions() {
 
         let id = 0;
-        questions.forEach(
-            question => {
-                let question_wrapper = document.createElement("div");
-                let view = id++ === 0 ? 'visible' : 'hidden';
-                let wrapper_id = question.name.toLowerCase();
-                question_wrapper.className = 'question-wrapper ' + view + ' question-' + wrapper_id
-                render_label(question, question_wrapper);
-                $("#insert-person").append(question_wrapper);
-                question.options.forEach(option => {
-                    let input_wrapper = document.createElement("div");
-                    input_wrapper.className = "col-12 question";
-                    let input_element = render_option(option, question.name);
-                    $(input_wrapper).append(input_element);
-                    let label_element = render_answer(question.category, option.value);
-                    label_element.for = input_element.id;
-                    $(input_wrapper).append(label_element);
-                    $(question_wrapper).append(input_wrapper);
-                });
+        questions.forEach(question => {
+            let question_wrapper = document.createElement("div");
+            let view = id++ === 0 ? 'visible' : 'hidden';
+            let wrapper_id = question.name.toLowerCase();
+            question_wrapper.className = 'question-wrapper ' + view + ' question-' + wrapper_id
+            render_label(question, question_wrapper);
+            $("#insert-person").append(question_wrapper);
+            question.options.forEach(option => {
+                let input_wrapper = document.createElement("div");
+                input_wrapper.className = "col-12 question";
+                let input_element = render_option(option, question.name);
+                $(input_wrapper).append(input_element);
+                let label_element = render_answer(question.category, option.value);
+                label_element.for = input_element.id;
+                $(input_wrapper).append(label_element);
+                $(question_wrapper).append(input_wrapper);
             });
+        });
     }
 
     function render_quiz() {
-        quiz_questions.forEach(
-            question => {
-                let question_label = document.createElement("h4");
-                question_label.innerHTML = question.text;
-                $("#insert-results").append(question_label);
-                let input_wrapper = document.createElement("div");
-                let input_element = document.createElement("input");
-                input_element.id = question.name;
-                input_element.name = question.name;
-                input_element.type = "text";
-                $(input_wrapper).append(input_element);
-                let label_element = document.createElement("label");
-                label_element.for = input_element.id;
-                $(input_wrapper).append(label_element);
-                $("#insert-results").append(input_wrapper);
-            });
+        quiz_questions.forEach(question => {
+            let question_label = document.createElement("h4");
+            question_label.innerHTML = question.text;
+            $("#insert-results").append(question_label);
+            let input_wrapper = document.createElement("div");
+            let input_element = document.createElement("input");
+            input_element.id = question.name;
+            input_element.name = question.name;
+            input_element.type = "text";
+            $(input_wrapper).append(input_element);
+            let label_element = document.createElement("label");
+            label_element.for = input_element.id;
+            $(input_wrapper).append(label_element);
+            $("#insert-results").append(input_wrapper);
+        });
     }
 
     function process_form() {
@@ -154,66 +155,65 @@
         let difference = 0;
         let total_score = 0;
 
-        $('form').serializeArray().forEach(
-            answer => {
-                let question_object = questions.filter(x => x.name === answer.name)
-                let quiz_object = quiz_questions.filter(x => x.name === answer.name)
-                if (question_object.length > 0) {
-                    let right_option = question_object[0].options.filter(x => x.name === answer.value)
-                    visual_value += right_option[0].visual_value
-                    static_value += right_option[0].static_value
-                    emotional_value += right_option[0].emotional_value
-                    interactive_value += right_option[0].interactive_value
+        $('form').serializeArray().forEach(answer => {
+            let question_object = questions.filter(x => x.name === answer.name)
+            let quiz_object = quiz_questions.filter(x => x.name === answer.name)
+            if (question_object.length > 0) {
+                let right_option = question_object[0].options.filter(x => x.name === answer.value)
+                visual_value += right_option[0].visual_value
+                static_value += right_option[0].static_value
+                emotional_value += right_option[0].emotional_value
+                interactive_value += right_option[0].interactive_value
 
-                }
-                if (quiz_object.length > 0) {
-                    max_score += 100
-                    if (Number(answer.value)) {
-                        difference = Math.abs(Number(quiz_object[0].right_answer) - Number(answer.value))
-                        let percentage = Number(quiz_object[0].right_answer) / Number(answer.value);
-                        let percentage_difference = Math.abs(100 - (percentage * 100))
-                        // if there is deviation but no big we will give points
-                        if (percentage_difference < 20) {
-                            total_score += (100) * ((100 - percentage_difference) / 100)
-                        }
-                    }
-                    // if is answer a string only right answer is considered as valid
-                    else {
-                        if (quiz_object[0].right_answer == answer.value) {
-                            total_score += 100;
-                        }
+            }
+            if (quiz_object.length > 0) {
+                max_score += 100
+                if (Number(answer.value)) {
+                    difference = Math.abs(Number(quiz_object[0].right_answer) - Number(answer.value))
+                    let percentage = Number(quiz_object[0].right_answer) / Number(answer.value);
+                    let percentage_difference = Math.abs(100 - (percentage * 100))
+                    // if there is deviation but no big we will give points
+                    if (percentage_difference < 20) {
+                        total_score += (100) * ((100 - percentage_difference) / 100)
                     }
                 }
+                // if is answer a string only right answer is considered as valid
+                else {
+                    if (quiz_object[0].right_answer == answer.value) {
+                        total_score += 100;
+                    }
+                }
+            }
 
-            });
-	    let steps = process_steps();
-		let value = {
-    		"success_rate": (total_score / max_score ) * 100,
-    		"visual_value":visual_value,
-    		"static_value":static_value,
-    		"emotional_value":emotional_value,
-    		"interactive_value":interactive_value,
-    		"steps": steps
-		}
-		console.log(value)
-		return value;
+        });
+        let steps = process_steps();
+        let value = {
+            "success_rate": (total_score / max_score) * 100,
+            "visual_value": visual_value,
+            "static_value": static_value,
+            "emotional_value": emotional_value,
+            "interactive_value": interactive_value,
+            "steps": steps
+        }
+        console.log(value)
+        return value;
     }
 
-	function process_steps() {
+    function process_steps() {
         let step_count = 1;
         let step_elements = $('.step');
         let steps_json = []
-        $('.step').each(function(obj, element) {
+        $('.step').each(function (obj, element) {
             steps_json.push({
                 "step_count": step_count,
-                "step_value":  $(element).children(".step_value")[0].value,
-                "step_comment":  $(element).children(".step_comment")[0].value,
-                "step_type":  $(element).children(".step_type")[0].value,
+                "step_value": $(element).children(".step_value")[0].value,
+                "step_comment": $(element).children(".step_comment")[0].value,
+                "step_type": $(element).children(".step_type")[0].value,
                 "step_time": $(element).children("span")[0].innerText
             });
         });
         return steps_json
-	}
+    }
 
     function call_ajax(data) {
         $.ajax({
@@ -223,13 +223,13 @@
             success: function (data, textStatus, jqXHR) {
                 //data - response from server
                 console.log(data, textStatus, jqXHR);
-                $('#main').load('network.html', function() {
+                $('#main').load('network.html', function () {
                     $('.main-header-content').html('<p>data</p>')
                 });
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 // render error
-                $('#main').load('network.html', function() {
+                $('#main').load('network.html', function () {
                     $('.main-header-content').html('<p>error</p>')
                 });
             }
@@ -237,9 +237,14 @@
     }
 
     function countdown(elementName, minutes, seconds) {
-        var element, endTime, hours, mins, msLeft, time;
+        var element,
+            endTime,
+            hours,
+            mins,
+            msLeft,
+            time;
 
-        function twoDigits(n){
+        function twoDigits(n) {
             return (n <= 9 ? "0" + n : n);
         }
 
@@ -252,10 +257,10 @@
                 $('#render_step_btn').addClass('disabled');
                 $('#save_step_btn').addClass('disabled');
             } else {
-                time = new Date( msLeft );
+                time = new Date(msLeft);
                 hours = time.getUTCHours();
                 mins = time.getUTCMinutes();
-                element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits(time.getUTCSeconds());
+                element.innerHTML = (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
                 setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
             }
         }
@@ -272,115 +277,14 @@
         //Render Questions & Quiz
         render_questions();
         render_quiz();
-
-        $("#render_step_btn").on("click", function () {
-            render_step();
-        })
-        $("#save_step_btn").on("click", function () {
-            save_button();
-        })
-        $("#process_form_btn").on("click", function () {
-			var data = process_form();
-            call_ajax(data);
-        })
-        $("input[type=radio]").on('change', function () {
-            let $checked = $("input[type=radio]:checked");
-            if ($checked.length === 4) {
-                let values = {
-                    'interactive_value': 0,
-                    'emotional_value': 0,
-                    'static_value': 0,
-                    'visual_value': 0,
-                };
-
-                $checked.each(function () {
-                        let data = $(this).data();
-                        values.interactive_value += data.interactive_value
-                        values.emotional_value += data.emotional_value
-                        values.static_value += data.static_value
-                        values.visual_value += data.visual_value
-                    }
-                );
-                const maxKey = _.max(Object.keys(values), o => values[o]);
-    function get_highest_attr($checked) {
-        let values = {
-            'interactive_value': 0,
-            'emotional_value': 0,
-            'static_value': 0,
-            'visual_value': 0,
-        };
-        $checked.each(function () {
-                let data = $(this).data();
-                values.interactive_value += data.interactive_value
-                values.emotional_value += data.emotional_value
-                values.static_value += data.static_value
-                values.visual_value += data.visual_value
-            }
-        );
-        return _.max(Object.keys(values), o => values[o]);
-    }
-
-                var title = "Improve your learning";
-                var bg_color = 'unset';
-
-                switch (maxKey) {
-                    case 'interactive_value':
-                        break;
-                    case 'emotional_value':
-                        break;
-                    case 'static_value':
-                        title = "Improve your brain";
-                        break;
-                    case 'visual_value':
-                        bg_color = 'rgba(234, 88, 56, 0.95)';
-                        break;
-                }
-
-                $('.landing-content h1').text(title);
-                $('.content-bg-wrap').css({'background-color': bg_color});
-            }
-            if ($checked.length === 5) {
-                $('#person-icon').addClass('disabled');
-                $('#learning-icon').removeClass('disabled');
-                countdown("ten-countdown", 1, 0);
-            }
-        })
-        $('input')
-    function attribute_action(maxKey) {
-        var title = "Improve your learning";
-        var bg_color = 'unset';
-        console.log(maxKey);
-        switch (maxKey) {
-            case 'interactive_value':
-                $('.landing-content *').addClass('growing');
-                break;
-            case 'emotional_value':
-                // TODO Well, think of something.
-                break;
-            case 'static_value':
-                title = "Improve your brain";
-                break;
-            case 'visual_value':
-                bg_color = 'rgba(234, 88, 56, 0.95)';
-                break;
-        }
-        $('.landing-content h1').text(title);
-        $('.content-bg-wrap').css({'background-color': bg_color});
-    }
-
-    $document.ready(function () {
-        var steps = 0;
-        //Render Questions & Quiz
-        render_questions();
-        render_quiz();
-
-        // Events
+        let steps = 0;
         $("#render_step_btn").on("click", function () {
             let add_new = true
             if (steps >= 1 && $(this).data().save !== 'False') add_new = save_button();
             if (add_new) render_step(++steps);
 
         });
+
         $("#remove_step_btn").on("click", function () {
             $('.step').last().remove();
             steps--;
@@ -400,7 +304,57 @@
         $("#process_form_btn").on("click", function () {
             var data = process_form();
             call_ajax(data);
-        });
+        })
+
+        function attribute_action(maxKey) {
+            var title = "Improve your learning";
+            var bg_color = 'unset';
+            console.log(maxKey);
+            switch (maxKey) {
+                case 'interactive_value':
+                    $('.landing-content *').addClass('growing');
+                    break;
+                case 'emotional_value':
+                    // TODO Well, think of something.
+                    break;
+                case 'static_value':
+                    title = "Improve your brain";
+                    break;
+                case 'visual_value':
+                    bg_color = 'rgba(234, 88, 56, 0.95)';
+                    break;
+            }
+            $('.landing-content h1').text(title);
+            $('.content-bg-wrap').css({'background-color': bg_color});
+        }
+
+        function get_highest_attr($checked) {
+            let values = {
+                'interactive_value': 0,
+                'emotional_value': 0,
+                'static_value': 0,
+                'visual_value': 0,
+            };
+            $checked.each(function () {
+                    let data = $(this).data();
+                    values.interactive_value += data.interactive_value
+                    values.emotional_value += data.emotional_value
+                    values.static_value += data.static_value
+                    values.visual_value += data.visual_value
+                }
+            );
+            return _.max(Object.keys(values), o => values[o]);
+        }
+
+        $('.container').on('mouseover', '.growing', function () {
+            let font_size = parseInt($(this).css('font-size'), 10);
+            $(this).animate(
+                {'fontSize': font_size + 10 + "px"},
+                'easeInOutCubic',
+                function () {
+                }
+            );
+        })
 
         $("input[type=radio]").on('change', function () {
             let $checked = $("input[type=radio]:checked");
@@ -413,24 +367,14 @@
             if ($closest.length !== 0) {
                 $wrapper.toggleClass('hidden visible')
                 $closest.toggleClass('visible hidden')
-            } else $('.nav-tabs li:eq(1) a').tab('show')
+            } else $('.nav-tabs li:eq(2) a').tab('show')
 
+            if ($checked.length === 5) {
+                $('#person-icon').addClass('disabled');
+                $('#learning-icon').removeClass('disabled');
+                countdown("ten-countdown", 1, 0);
+            }
 
         });
-
-        $('.container')
-            .on('mouseover', '.growing', function () {
-
-                let font_size = parseInt($(this).css('font-size'), 10);
-
-
-                $(this).animate(
-                    {'fontSize': font_size + 10 + "px"},
-                    'easeInOutCubic',
-                    function () {
-                    }
-                );
-            })
-
     });
 })(jQuery);
